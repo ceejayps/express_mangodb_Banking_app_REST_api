c1de2ee3c8fea0f06716089b65fbabdaadd668ff
ceejayps <ceejayps1308@gmail.com>
2022-02-28 13:13:47 -0500

    initiate server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("h")
+const express = require("express");
+const server = express();
+
+const bcrypt = require("bcrypt");
+const expressLayouts = require("express-ejs-layouts");
+
+server.set('view engine', 'ejs')
+server.set('views', __dirname+'/views')
+server.set('layout', 'layouts/layout')
+server.use(express.static('public'))
+
+server.listen(process.env.Port || 1876)
