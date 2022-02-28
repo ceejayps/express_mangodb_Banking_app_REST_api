992b6589dfabdf85faaf7e383dce859c4689724c
ceejayps <ceejayps1308@gmail.com>
2022-02-28 15:44:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+if(process.env.NODE_ENV !=="production"){
+    require('dotenv').config({ path: '.env' });
+}
+
 const express = require("express");
 const server = express();
-
 const bcrypt = require("bcrypt");
 const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require('./routes/index');
+const mongoose  = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+const db = mongoose.connection;
+db.on("error", error=>console.error(error))
+db.once("open", ()=>console.log("connected to db"))
 
-const indexRouter = require('./routes/index')
 
 server.set('view engine', 'ejs')
 server.set('views', __dirname+'/views')
@@ -12,5 +20,7 @@ server.set('layout', 'layouts/layout')
 server.use(expressLayouts)
 server.use(express.static('public'))
 server.use('/',indexRouter)
+
+
 
 server.listen(process.env.Port || 1876)
