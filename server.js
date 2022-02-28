ce2c189296db0ba2bd21c38cd3d7eab01c2d344a
ceejayps <ceejayps1308@gmail.com>
2022-02-28 13:39:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const server = express();
 const bcrypt = require("bcrypt");
 const expressLayouts = require("express-ejs-layouts");
 
+const indexRouter = require('./routes/index')
+
 server.set('view engine', 'ejs')
 server.set('views', __dirname+'/views')
 server.set('layout', 'layouts/layout')
 server.use(express.static('public'))
+server.use('/',indexRouter)
 
 server.listen(process.env.Port || 1876)
