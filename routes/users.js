3e2973328fa8dc20b79628fb6cf963971145ce15
ceejayps <ceejayps1308@gmail.com>
2022-02-28 18:02:54 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const user = require("../models/user")
 const router = express.Router(); 
+
+//get all users
 router.get('/', (req,res)=>{
 res.send("ok")
 })
+
+//get one users
+router.get('/:id', (req,res)=>{
+    res.send("ok")
+    })
+
+//create one users
+router.post('/', (req,res)=>{
+    res.send("ok")
+    })
+
+//update one users
+router.post('/:id', (req,res)=>{
+    res.send("ok")
+    })
+
+//delete one users
+router.delete('/:id', (req,res)=>{
+    res.send("ok")
+    })
+
 
  module.exports = router;
