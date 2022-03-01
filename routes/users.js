a26bb86ff3351d0dca6691125d08ba0c43288a9e
ceejayps <ceejayps1308@gmail.com>
2022-02-28 19:45:38 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ const user = require("../models/user")
 const router = express.Router(); 
 
 //get all users
-router.get('/', (req,res)=>{
+router.get('/', async (req,res)=>{
+
+    try {
+        const users = await user.find()
+        res.json(users);
+    } catch (e) {
+        return res.status(500).json({message:e})
+    }
 res.send("ok")
 })
 
