5937ea02eaca47e0a0a8f057df5cc63d89e28d6b
ceejayps <ceejayps1308@gmail.com>
2022-03-01 19:59:02 -0500

    Update tranactions.js

--- routes/tranactions.js
+++ routes/tranactions.js
@@ -6,7 +6,11 @@ const express = require("express");
 const transaction = require("../models/transaction");
 const router = express.Router(); 
 
-
+//getall
+router.get('/', async(req,res)=>{
+    const  transactions = await transaction.find();
+ })
+ 
 
 
 module.exports = router;
