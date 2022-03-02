3984391c221df3b7700bed9dc9e34576b623fb0a
ceejayps <ceejayps1308@gmail.com>
2022-03-02 06:41:06 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ router.post('/login', async (req,res)=>{
          const user = await User.find({ accountNumber:req.body.identifier})
          try  {
             if(await bcrypt.compare(req.body.password, user[0].password)){
-                 let JWT = jwt.sign({id:user[0]._id, email:user[0].email, accountNumber:user[0].accountNumber, role:user[0].role, fullname:user[0].fullname}, process.env.ACCESS_TOKEN_SECRET);
+                 let JWT = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
               return  res.status(200).json([JWT, user[0]])
             }
             else{
