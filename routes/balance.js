222d6c672320ad6136ac449f0f78669e889e1b62
ceejayps <ceejayps1308@gmail.com>
2022-03-02 11:57:56 -0500

    Update balance.js

--- routes/balance.js
+++ routes/balance.js
@@ -7,6 +7,24 @@ const user = require("../models/user");
 const router = express.Router(); 
 
 
+router.post('/',async (req,res)=>{
+
+})
+
+
+
+/*----------------- middleware user permission ----------------*/
+function Autherize(req,res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(token == null){  console.log('stuch step 3'); return res.sendStatus(401)}
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        if(err){return res.sendStatus(403)}
+        req.user = user;
+        req.params.id = req.params.id;
+        next();
+    })
+}
 
 
 module.exports = router;
