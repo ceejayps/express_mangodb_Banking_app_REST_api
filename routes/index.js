3ec3387f0da6cffad2104647a5b162373b367682
ceejayps <ceejayps1308@gmail.com>
2022-03-02 04:01:31 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -125,7 +125,7 @@ router.post('/register', async(req,res)=>{
             
             currentUser.confirmed = true;
             currentUser.role ='authenticated'
-            currentUser.confirmationToken = 'n/a'
+            currentUser.confirmationToken = 'nan'
 
 
             try {
