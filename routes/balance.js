951a85ef2036f9e8eb2293f94cb9fc9fcc8526b4
ceejayps <ceejayps1308@gmail.com>
2022-04-01 11:14:00 -0500

    Update balance.js

--- routes/balance.js
+++ routes/balance.js
@@ -11,19 +11,15 @@ const router = express.Router();
 router.post('/',Autherize,async (req,res)=>{
    // console.log(req.user._id, req.user.accountNumber, req.user.email ,{req:req.receipient})
     const sender = await user.findById(req.user._id)
-    if( req.receipient === req.user.accountNumber||req.receipient === req.user.email){
-        return res.status(400).json({message:"you can't be the receipient"})
-    }else{
+    if( req.receipient === req.user.accountNumber||req.receipient === req.user.email)return res.status(400).json({message:"you can't be the receipient"})
+    
         try {
 if(req.isemail){
     try {
 
         const receipient = await user.findOne({email:req.receipient})
-        if(receipient == null){
-            res.sendStatus(404)
-        }
-        else{
-            if(sender.Balance >=req.body.amount ){
+        if(receipient == null)res.sendStatus(404)
+            if(sender.Balance < req.body.amount) return res.status(400).json({message:"invalid amount"})
                 sender.Balance = 100000;
             receipient.Balance += req.body.amount;
           const  newSender = await sender.save()
@@ -34,48 +30,17 @@ if(req.isemail){
               senderName: sender.fullname,
               recipientAccountNumber: receipient.accountNumber,
               recipientName: receipient.fullname,
-              Amount: req.body.amount,
-             
-              
-        })
-
-     
-
-try {
-    
-        
+              Amount: req.body.amount,    
+        }); try {
           await  NewTransac.save()
-
-            res.status(200).json(NewTransac)
+        res.status(200).json(NewTransac)
         } catch (error) {
             res.status(500).json(error)
         }
-          
-         
-
-
-
-
-
-           
-            }
-        }
    
-    } catch (error) {
-        res.sendStatus(500)
-        
-    }
-    
-}else{
-
-}
-
-            
-        } catch (error) {
-            
-        }
-    }
-
+    } catch (error) {res.sendStatus(500)}
+} 
+} catch (error) {}
 })
 
 
