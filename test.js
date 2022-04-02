901c33e267b9653b584384d5a33e80690010f1c4
ceejayps <ceejayps1308@gmail.com>
2022-04-02 08:12:31 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,27 @@
 function solution(sequence) {
-    let goal = 0;
-let char = sequence.split('');
-console.log(char)
-char.forEach (  function async(elem)  {
+let goal = 0;
+let char = sequence.split('').forEach (  function (elem)  {
   if (elem == '(' ){
-      goal += 1;
+    goal += 10;
+    if(goal < 0) return false
   }
-  else if (elem == '[' ){
-      goal += 2;
+  else if (elem == '[' ){goal += 20;
+    if(goal < 0 || goal%2 !=0) return false
   }
-  else if(elem == '{' ){
-      goal += 3;
+  else if(elem == '{' ){goal += 3;
+    if(goal < 0 || goal%2 !=0) return false   
   }
- else  if(elem == ')' ){
-      goal -= 1;
+ else  if(elem == ')' ){ goal -= 10;
+      if(goal < 0 || goal%2 !=0) return false
   }
-  else if(elem == ']' ){
-      goal -= 2;
+  else if(elem == ']' ){goal -= 20;
+      if(goal < 0 || goal%2 !=0) return false
   }
-    else  if(elem == '}' ){
-      goal -= 3;
-  }
-   
-
-  
-});
-if (goal < 0){ console.log( "false")}
+    else  if(elem == '}' ){ goal -= 3;
+      if(goal < 0 || goal%2 !=0) return false
+  }});
+if (goal != 0){ console.log( "false")}
     else { console.log( "true")}
-
+    console.log(`final ${goal}`)
 }
+solution("((()))")
